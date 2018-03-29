ec4216fe31650cfcc4e0a8ec01206af5526688b9
ozin <b1506231@planet.kanazawa-it.ac.jp>
2018-03-29 17:14:28 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,74 +1,107 @@
 $ = function (x) {
-    return document.getElementById(x);
+  return document.getElementById(x);
 }
 
-var xmlHttp;
+var n = 1;
 var members = new Array();
-var token = "Your slack token!";
+var membersUse = new Array();
 
-xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "https://slack.com/api/users.list?token=" + token, false);
-xmlHttp.send(null);
-data = xmlHttp.responseText;
-json = JSON.parse(data);
-users = json['members'];
+function paseJson() {
+  var xmlHttp;
+  var token = "your slack token";
+  var isStop = true;
 
-for (i = 0; i < 30; i++) {
-    members[i] = (users[i]['name']);
-    var numList = members
-    var isStop = true;
-    var n = 1;
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", "https://slack.com/api/users.list?token=" + token, false);
+  xmlHttp.send(null);
+  data = xmlHttp.responseText;
+  json = JSON.parse(data);
+  users = json['members'];
 
-    function startBingo() {
-        $("start").style.display = "none";
-        $("stop").style.display = "inline";
-        isStop = false;
-        roulette();
+  for (let user of users) {
+    if (user['deleted'] == false) {
+      members.push((user['name']));
     }
+  }
 
-    function stopBingo() {
-        $("start").style.display = "inline";
-        $("stop").style.display = "none";
-        isStop = true;
+  return members
+}
+
+
+function initCheckbox() {
+  members = paseJson();
+  for (member of members) {
+    var box = $('box');
+    box.insertAdjacentHTML('afterbegin', `<input type="checkbox" checked="checked" name="user" value="${member}" class="select-user"><label>${member}</label><br>`)
+  }
+}
+
+function getCheckbox() {
+  var checkbox = document.getElementsByName('user')
+  for (i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked == true) {
+      membersUse.push(checkbox[i].value);
     }
+  }
+  alert('追加されました');
+  return membersUse;
+}
 
-    function resetBingo() {
-        location.reload();
+
+function startBingo() {
+  if (membersUse.length == 0) {
+    alert('メンバーを選択してください');
+  } else {
+    $("start").style.display = "none";
+    $("stop").style.display = "inline";
+    isStop = false;
+    roulette();
+  }
+}
+
+function stopBingo() {
+  $("start").style.display = "inline";
+  $("stop").style.display = "none";
+  isStop = true;
+}
+
+function resetBingo() {
+  location.reload();
+}
+
+
+function roulette() {
+  var id = "";
+  var rnd = Math.floor(Math.random() * membersUse.length);
+
+  if (isStop) {
+    clearTimeout(id);
+    $("view").innerText = membersUse[rnd];
+    if (!$("out").innerText) {
+      var newli = document.createElement("li");
+      var list = $("out");
+      var member = n + "：" + membersUse[rnd];
+      list.append(newli);
+      newli.append(member);
+    } else {
+      n++;
+      var newli = document.createElement("li");
+      var list = $("out");
+      var member = n + "：" + membersUse[rnd];
+      list.append(newli);
+      newli.append(member);
     }
+    membersUse.splice(rnd, 1);
 
-    function roulette() {
-        var id = "";
-        var rnd = Math.floor(Math.random() * numList.length);
-
-        if (isStop) {
-            clearTimeout(id);
-            $("view").innerText = numList[rnd];
-            if (!$("out").innerText) {
-                var newli = document.createElement("li");
-                var list = $("out");
-                var member = n + "：" + numList[rnd];
-                list.append(newli);
-                newli.append(member);
-            } else {
-                n++;
-                var newli = document.createElement("li");
-                var list = $("out");
-                var member = n + "：" + numList[rnd];
-                list.append(newli);
-                newli.append(member);
-            }
-            numList.splice(rnd, 1);
-
-            if (numList.length == 0) {
-                alert("Finish");
-                $("start").disabled = true;
-                $("start").style.display = "none";
-                $("reset").style.display = "inline";
-            }
-            return false;
-        }
-
-        $("view").innerText = numList[rnd];
-        id = setTimeout("roulette()", 10);
+    if (membersUse.length == 0) {
+      alert("Finish");
+      $("start").disabled = true;
+      $("start").style.display = "none";
+      $("reset").style.display = "inline";
     }
+    return false;
+  }
+
+  $("view").innerText = membersUse[rnd];
+  id = setTimeout("roulette()", 10);
 }
